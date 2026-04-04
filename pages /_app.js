010d12b34e91b78404ff9ddc0891e93120070403
AHMAD SULEIMAN MUSALLAM AlHawawsheh <38798371+hawawsha@users.noreply.github.com>
2026-04-04 15:54:58 +0300

    Update _app.js

--- pages /_app.js
+++ pages /_app.js
@@ -1,1 +1,9 @@
-٣
+import { PiPriceProvider } from '../context/PiPriceContext';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <PiPriceProvider>
+      <Component {...pageProps} />
+    </PiPriceProvider>
+  );
+}
