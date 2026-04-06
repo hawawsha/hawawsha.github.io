5f70ade85342d79c580cfe44c758b900481f884d
AHMAD SULEIMAN MUSALLAM AlHawawsheh <38798371+hawawsha@users.noreply.github.com>
2026-04-06 22:34:32 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -44,7 +44,7 @@ export default function Home() {
   useEffect(() => {
     const interval = setInterval(() => {
       if (typeof window !== 'undefined' && window.Pi) {
-      window.Pi.init({ version: "2.0", sandbox: true }, {
+      window.Pi.init({ version: "2.0", sandbox: false }, {
           onIncompletePaymentFound: async (payment) => {
             try {
               await fetch('/api/payment', {
