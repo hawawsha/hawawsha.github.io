b412157924ddde84e840a462da02cc7da49663df
AHMAD SULEIMAN MUSALLAM AlHawawsheh <38798371+hawawsha@users.noreply.github.com>
2026-04-16 22:32:43 +0300

    Update products.js

--- pages/api/products.js
+++ pages/api/products.js
@@ -1,13 +1,11 @@
-// بدلاً من كتابة الـ ID يدوياً، نستخدم عملية الاستدعاء المؤمنة
 const baseId = process.env.AIRTABLE_BASE_ID;
-const apiKey = process.env.AIRTABLE_API_KEY;
+const apiKey = process.env.AIRTABLE_TOKEN;
 
 export default async function handler(req, res) {
   const { table } = req.query;
-  
-  // الرابط سيصبح الآن ديناميكي وآمن
+
   const url = `https://api.airtable.com/v0/${baseId}/${table}`;
-  
+
   try {
     const response = await fetch(url, {
       headers: { Authorization: `Bearer ${apiKey}` }
